c56858d10d42d9dd4ff2ba5170206fa1415346f5
quanglinhh <93904360+quanglinhh@users.noreply.github.com>
2022-07-07 10:42:30 +0700

    .
    .

--- JavaEx/JavaCore1/src/Ex1/Ex1_p2.java
+++ JavaEx/JavaCore1/src/Ex1/Ex1_p2.java
@@ -1,55 +1,162 @@
 package Ex1;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Ex1_p2 {
     public static void main(String[] args) {
-        List<Integer> A = new ArrayList<Integer>();
-        List<Integer> B = new ArrayList<Integer>();
+        List<Integer> A = new ArrayList<>();
+        List<Integer> B = new ArrayList<>();
         nhapMang((ArrayList<Integer>) A,"nhất");
         nhapMang((ArrayList<Integer>) B,"hai");
+        /*
         A.sort(Comparator.naturalOrder());
         B.sort(Comparator.reverseOrder());
         System.out.println("Mảng sau khi được săp xếp là");
         System.out.println(A);
         System.out.println(B);
         System.out.println(A + " " + B);
-
-
+         */
+        //sortA((ArrayList<Integer>) A,0,A.size()-1);
+        //Chuyển 2 arrayLít thành 2 mảng
+        int[] arrA = new int[A.size()];
+        int[] arrB = new int[B.size()];
+        //Nhập vào giá trị 2 mảng
+        for (int i = 0 ; i < B.size(); i++){
+            arrB[i] = B.get(i);
+        }for (int i = 0 ; i < A.size(); i++){
+            arrA[i] = A.get(i);
+        }
+        //Sắp xếp
+        sortB(arrB,0,B.size()-1);
+        sortA(arrA,0,A.size()-1);
+        //Gán lại giá trị 2 arrlayLít
+        A.clear();
+        B.clear();
+        for(int i = 0; i < arrA.length; i++){
+            A.add(arrA[i]);
+        }
+        for(int i = 0; i < arrB.length; i++){
+            B.add(arrB[i]);
+        }
+        System.out.println("Mảng A sau khi được săp xếp là");
+        System.out.println(A);
+        System.out.println("Mảng B sau khi được săp xếp là");
+        System.out.println(B);
+        for(int i = 0; i < arrB.length; i++){
+            A.add(arrB[i]);
+        }
+        System.out.println("Nối 2 mảng");
+        System.out.println(A);
+        /*In ra các phần từ mà A và B đều có (Viết phương thức intersection())*/
+        HashSet<Integer> set = new HashSet<>();
+       for(int val: arrA){
+           set.add(val);
+       }
+        System.out.println("Các phần tử trùng nhau: ");
+       for(int val : arrB){
+           if(set.contains(val)){
+               System.out.print(val+", ");
+           }
+       }
     }
-    public static void merge(ArrayList<Integer> arr, int l,int m, int r ){
+    public static void mergeA(int[] arr, int l,int m, int r ){
         int n1 = m - l+1;
         int n2 = r - m;
+        //Chuyen arrayList sang array
 
-        ArrayList<Integer> tempL = new ArrayList<Integer>(n1);
-        ArrayList<Integer> tempR = new ArrayList<Integer>(n2);
-
-        for(int i = 0 ; i <= tempL.size()-1; i++){
-            tempL.add(arr.get(l+i));
+        int[] tempL = new int[n1];
+        int[] tempR = new int[n2];
+        for(int i = 0 ; i <= tempL.length-1; i++){
+            tempL[i] = arr[l+i];
         }
-        for(int i = 0 ; i <= tempL.size()-1; i++){
-            tempR.add(arr.get(m+1+i));
+        for(int i = 0 ; i <= tempL.length-1; i++){
+            tempR[i] = arr[m+1+i];
         }
-
         int i = 0, j = 0;
         int k = l;
         while (i < n1 && j < n2){
-            if(tempL.get(i) <= tempR.get(j)){
-                arr.get(k) = tempL.get(i);
+            if(tempL[i] <= tempR[j]){
+                arr[k] = tempL[i];
+                i++;
             }
+            else{
+                arr[k] = tempR[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1){
+            arr[k] = tempL[i];
+            i++;
+            k++;
+        }
+        while (j < n2){
+            arr[k] = tempR[j];
+            j++;
+            k++;
         }
 
-        tempL.sort(Comparator.naturalOrder());
-        tempR.sort(Comparator.reverseOrder());
 
-        System.out.println(tempL + " " + tempR);
+
     }
-    public static void sort(ArrayList<Integer> arr, int l, int r ){
+    public static void mergeB(int[] arr, int l,int m, int r ){
+        int n1 = m - l+1;
+        int n2 = r - m;
+
+
+        ///
+        int[] tempL = new int[n1];
+        int[] tempR = new int[n2];
+        //Add gia tri vao 2 bang
+        for(int i = 0; i < n1; i++){
+            tempL[i] = arr[l+i];
+        }
+        for(int i = 0; i < n2; i++){
+            tempR[i] = arr[m+1+i];
+        }
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2){
+            if(tempL[i] >= tempR[j]){
+                arr[k] = tempL[i];
+                i++;
+            }else {
+                arr[k] = tempR[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < n1){
+            arr[k] = tempL[i];
+            i++;
+            k++;
+        }
+        while (j < n2){
+            arr[k] = tempR[j];
+            j++;
+            k++;
+        }
+
+
+    }
+
+    public static void sortA(int[] arr, int l, int r){
         if(l<r){
             int m = (l+r)/2;
-            sort( arr, l, m);
-            sort( arr, m+1, r);
-            merge(arr,l,m,r);
+            sortA( arr, l, m);
+            sortA( arr, m+1, r);
+            mergeA(arr,l,m,r);
+
+        }
+    }
+    public static void sortB(int[] arr, int l, int r){
+        if(l<r){
+            int m = (l+r)/2;
+            sortB( arr, l, m);
+            sortB( arr, m+1, r);
+            mergeB(arr,l,m,r);
+
         }
     }
     public static void nhapMang(ArrayList<Integer> arr,String a){
