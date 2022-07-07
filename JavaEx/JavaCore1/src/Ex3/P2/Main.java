a6559d5cfdb12f14c2f6ff995e7b65315b730efb
quanglinhh <93904360+quanglinhh@users.noreply.github.com>
2022-07-07 15:36:07 +0700

    .
    .

--- JavaEx/JavaCore1/src/Ex3/P2/Main.java
+++ JavaEx/JavaCore1/src/Ex3/P2/Main.java
@@ -1,24 +1,26 @@
 package Ex3.P2;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         HashMap<Integer,String> tinh = new HashMap<>();
         tinh.put(49,"Thành phố Hồ Chí Minh");
-        tinh.put(02,"Đà Nẵng");
+        tinh.put(2,"Đà Nẵng");
         tinh.put(11,"Cà Mau");
         tinh.put(22,"Nha Trang");
         tinh.put(100,"Hà Nội");
 
         System.out.println(tinh);
         System.out.println("----- Sau khi sắp xếp -----");
-        TreeMap<Integer, String> sorted = new TreeMap<Integer, String>(tinh);
+        TreeMap<Integer, String> sorted = new TreeMap<>(tinh);
         Set<Map.Entry<Integer, String>> mappings = sorted.entrySet();
         for (Map.Entry<Integer, String> mapping : mappings) {
+            System.out.print( mapping.getValue()+", ");
+        }
+        System.out.println("\n-------------------------------");
+        mappings.removeIf(mapping->mapping.getKey()%2!=0);
+        for (Map.Entry<Integer, String> mapping : mappings){
             System.out.print( mapping.getValue()+", ");
         }
     }
