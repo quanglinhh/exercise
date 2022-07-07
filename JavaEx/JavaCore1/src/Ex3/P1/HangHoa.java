2e689255a3526c99f463df018668231edeba63d3
quanglinhh <93904360+quanglinhh@users.noreply.github.com>
2022-07-07 14:10:12 +0700

    .
    .

--- JavaEx/JavaCore1/src/Ex3/P1/HangHoa.java
+++ JavaEx/JavaCore1/src/Ex3/P1/HangHoa.java
@@ -1,6 +1,7 @@
 package Ex3.P1;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class HangHoa {
@@ -15,7 +16,7 @@ public class HangHoa {
         this.ngaySanXuat = ngaySanXuat;
         this.ngayHetHan = ngayHetHan;
     }
-
+    public HangHoa(){}
     public int getMa() {
         return ma;
     }
@@ -36,19 +37,52 @@ public class HangHoa {
         return ngaySanXuat;
     }
 
-    public void setNgaySanXuat(Date ngaySanXuat) {
-        this.ngaySanXuat = ngaySanXuat;
-    }
 
+    public void setNgaySanXuat(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        this.ngaySanXuat = calendar.getTime();
+    }
     public Date getNgayHetHan() {
         return ngayHetHan;
     }
 
-    public void setNgayHetHan(Date ngayHetHan) {
-        this.ngayHetHan = ngayHetHan;
+    public void setNgayHetHan(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day);
+        this.ngayHetHan = calendar.getTime();
+    }
+    //kiểm tra tên hàng không được để trống
+    public boolean kiemTraTenHang(boolean k) {
+        if (this.ten == "" || this.ten.isEmpty()) {
+            System.out.println("Tên hàng không được để trống : ");
+        } else {
+            k = false;
+        }
+        return k;
+    }
+    //khởi tạo hàm kiểm tra ngày hết hạn không được nhỏ hơn ngày sản xuất
+    public boolean kiemTraNgay(boolean t) {
+        if (this.getNgaySanXuat().compareTo(this.getNgayHetHan()) < 0) {
+            t = false;
+        } else {
+            System.out.println("Ngày hết hạn không được nhỏ hơn ngày sản xuất : ");
+        }
+        return t;
+    }
+    //khởi tạo phương thức kiểm tra hạn sử dụng của sản phẩm đã hết hạn hay còn hạn
+    public void kiemTraHSD() {
+        Date today = new Date();
+        today.getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String st = simpleDateFormat.format(today);
+        if (this.getNgayHetHan().compareTo(today) < 0) {
+            System.out.println("Hôm nay là ngày " + st + ", hàng hóa đã hết hạn ");
+        } else {
+            System.out.println("Hôm nay là ngày " + st + ", hàng hóa vẫn còn hạn ");
+        }
     }
     SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-
     @Override
     public String toString() {
         return "Mã sản phẩm: "+ma+", Tên sản phẩm: "+ten+", Ngày sản xuất: "+df.format(ngaySanXuat)+", Ngày hết hạn: "+df.format(ngayHetHan);
--- JavaEx/JavaCore1/src/Ex3/P1/Main.java
+++ JavaEx/JavaCore1/src/Ex3/P1/Main.java
@@ -1,7 +1,29 @@
 package Ex3.P1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        HangHoa hangHoa = new HangHoa();
+        boolean kiemTraTen = true;
+        boolean kiemTraHan = true;
+        System.out.print("Nhập mã hàng: ");
+        int ma = scanner.nextInt();
+        hangHoa.setMa(ma);
+        scanner.nextLine();
+        do{
+            System.out.print("Nhập tên hàng: ");
+            String name = scanner.nextLine();
+            hangHoa.setTen(name);
+        }while (hangHoa.kiemTraTenHang(kiemTraTen));
+        do{
+            System.out.print("Nhập ngày sản xuất: ");
+            hangHoa.setNgaySanXuat(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            System.out.print("Nhập ngày hết hạn: ");
+            hangHoa.setNgayHetHan(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        }while (hangHoa.kiemTraNgay(kiemTraHan));
+        System.out.println(hangHoa);
+        hangHoa.kiemTraHSD();
     }
 }
