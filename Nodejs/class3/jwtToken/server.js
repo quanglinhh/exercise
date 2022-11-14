970241cb595a47c460e8778fdf685d638cfb1dad
quanglinhh <93904360+quanglinhh@users.noreply.github.com>
2022-11-14 17:07:29 +0700

    .
    .

--- Nodejs/class3/jwtToken/server.js
+++ Nodejs/class3/jwtToken/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
+const app = express();
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -20,8 +20,40 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
